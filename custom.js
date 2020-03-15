a8d901ca6a7e0c381b4fb237eb76b45d267bfba0
Sujan <sujan_khadgi2000@yahoo.com>
2020-03-14 20:00:32 -0400

    Added digital RSVP

--- custom.js
+++ custom.js
@@ -1,3 +1,35 @@
-var registry = document.getElementsByTagName("script")[0].parentNode;
-var frame = registry.getElementsByTagName("iframe");
-console.log(frame);
+$(".form-rsvp button").click(function(){
+	checkRSVP();
+})
+
+$('.rsvp-input').on('keydown', function(e) {
+    if (e.which == 13) {
+        e.preventDefault();
+        checkRSVP();
+    }
+});
+
+function checkRSVP(){
+	if(!$(".rsvp-input").val() ) {
+		$(".error").show().text("Please enter the code.");
+	}else if($(".rsvp-input").val() === "0301"){
+		window.open("https://forms.gle/zRU1mT1qQdFNfkkD9"); //plus one
+	}else if($(".rsvp-input").val() === "1019"){
+		window.open("https://forms.gle/KufcVtKfZc2CipMG8"); //single person
+	}else{
+		$(".error").show().text("Please enter a correct code.");
+	}
+}
+
+$(".rsvp-input").focus(function(event) {
+	/* Act on the event */
+	$(".error").hide();
+});
+
+$(document).ready(function (){
+	$("#rsvp").click(function (){
+		$('html, body').animate({
+			scrollTop: $(".rsvp").offset().top
+		}, 2000);
+	});
+});
